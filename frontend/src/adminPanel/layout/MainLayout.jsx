21ba2e35218db7a81f8951b5b6a35038199b2431
Shahadat Hossain Shamim <shamim131666@gmail.com>
2025-11-13 14:11:21 +0600

    Initial Sidebar
    Initial Sidebar Dashboard Setup

--- frontend/src/adminPanel/layout/MainLayout.jsx
+++ frontend/src/adminPanel/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom"
 import Sidebar from "./Sidebar"
 
 const MainLayout = () => {
   return (
-    <div className=" min-w-screen min-h-screen bg-slate-100">
-        <Sidebar/>
+    <div className=" min-w-screen min-h-screen bg-slate-300">
+      <Sidebar/>
+      <Outlet/>
       Main Layout
     </div>
   )
--- frontend/src/adminPanel/layout/Sidebar.jsx
+++ frontend/src/adminPanel/layout/Sidebar.jsx
@@ -1,12 +1,41 @@
-import { Outlet } from "react-router-dom"
+import { AiFillDashboard } from 'react-icons/ai';
+import { Link, useLocation } from 'react-router-dom';
+import bartabuzzlogo from '../../../../assets/BartaBuzzD1.png';
+
 
 const Sidebar = () => {
-  return (
-    <div>
-      Sidebar
-      <Outlet/>
-    </div>
-  )
-}
 
-export default Sidebar
+  const {pathname} = useLocation()
+
+  return (
+    <div className="w-[250px] h-screen fixed left-0 top-0 bg-white">
+      <div className="h-[70px] flex justify-center items-center">
+        <Link to='/'>
+          <img
+          className='w-[200px] h-[50px]'
+          src={bartabuzzlogo}
+          alt='BartaBuzz Logo'
+          />
+        </Link>
+      </div>
+      <ul className="px-3 flex flex-col gap-y1 font-medium">
+        <li>
+          <Link
+            to="/adminPanel/admin"
+            className={`${pathname === '/adminPanel/admin'
+            ? 'bg-blue-600 text-white'
+            : 'bg-white text-[#4040F6]'
+            } px-3 py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-sm flex gap-x-2 justify-start items-center hover:bg-blue-600 hover:text-white`}
+            >
+            <span className="text-xl">
+              <AiFillDashboard />
+            </span>
+            <span>Dashboard</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
